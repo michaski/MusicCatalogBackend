873889e6cc18f102e7509b107c69df49902cb315
michaski <michaski86@gmail.com>
2023-04-30 04:19:49 +0200

    Add validation

---
+++ MusicCatalog.Application/Albums/Validators/CreateAlbumValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using MusicCatalog.Application.Albums.Commands;
+
+namespace MusicCatalog.Application.Albums.Validators
+{
+    public class CreateAlbumValidator : AbstractValidator<CreateNewAlbumCommand>
+    {
+        public CreateAlbumValidator()
+        {
+            RuleFor(cmd => cmd.Artist)
+                .NotEmpty();
+            RuleFor(cmd => cmd.Title)
+                .NotEmpty();
+            RuleFor(cmd => cmd.TypeId)
+                .NotEmpty();
+            RuleFor(cmd => cmd.ReleaseYear)
+                .InclusiveBetween(1800, DateTime.Now.Year);
+            RuleForEach(dto => dto.Tracks)
+                .SetValidator(new NewTrackValidator());
+        }
+    }
+}
---
+++ MusicCatalog.Application/Albums/Validators/NewTrackValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using MusicCatalog.Application.Dtos.Tracks;
+
+namespace MusicCatalog.Application.Albums.Validators
+{
+    public class NewTrackValidator : AbstractValidator<NewTrackDto>
+    {
+        public NewTrackValidator()
+        {
+            RuleFor(dto => dto.Artist)
+                .NotEmpty();
+            RuleFor(dto => dto.Title)
+                .NotEmpty();
+            RuleFor(dto => dto.ReleaseYear)
+                .InclusiveBetween(1800, DateTime.Now.Year);
+            RuleFor(dto => dto.DurationMinutes)
+                .GreaterThanOrEqualTo(0);
+            RuleFor(dto => dto.DurationSeconds)
+                .ExclusiveBetween(0, 60);
+        }
+    }
+}
